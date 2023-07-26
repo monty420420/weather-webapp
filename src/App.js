2fb7b150bda4bd042ef07d6a6681e6f7bcdfbcf3
Muntaeyeon <monty420420@naver.com>
2023-07-26 16:23:50 +0900

    html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState, Suspense } from 'react';
+import axios from 'axios';
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <div className='header'>
+           <div className='header_select'></div>
+           <div className='header_search'></div>
+         </div>
     </div>
   );
 }
